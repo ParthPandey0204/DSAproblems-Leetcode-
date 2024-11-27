0abe6efdede945e398f0a9188a2df012395d865f
Parth Pandey <parth.pandey80045@gmail.com>
2024-11-27 23:03:41 +0530

    Time: 7 ms (22.59%) | Memory: 23.8 MB (9.23%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     string reverseWords(string s) {
-          reverse(s.begin(), s.end());
-        int n = s.size();
-        int left = 0;
-        int right = 0;
-        int i = 0;
-        while (i < n) {
-            while (i < n && s[i] == ' ')
-                i++;
-            if (i == n)
-                break;
-            while (i < n && s[i] != ' ') {
-                s[right++] = s[i++];
+        // Remove leading spaces
+        removeLeadingSpaces(s);
+        
+        string temp = "";
+        string ans = "";
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != ' ') {
+                temp += s[i];
+            } else if (s[i] == ' ') {
+                if (!temp.empty()) {
+                    if (!ans.empty()) {
+                        ans = temp + " " + ans;
+                    } else {
+                        ans = temp;
+                    }
+                    temp = "";
+                }
             }
-            reverse(s.begin() + left, s.begin() + right);
-            s[right++] = ' ';
-            left = right;
-            i++;
         }
-        s.resize(right - 1);
-        return s;
+        
+        if (!temp.empty()) {
+            if (!ans.empty()) {
+                ans = temp + " " + ans;
+            } else {
+                ans = temp;
+            }
+        }
+        
+        return ans;
+    }
+
+private:
+    void removeLeadingSpaces(string& s) {
+        size_t start = s.find_first_not_of(' ');
+        if (start != string::npos) {
+            s = s.substr(start);
+        } else {
+            s = ""; // If all spaces, return an empty string
+        }
     }
 };
+
