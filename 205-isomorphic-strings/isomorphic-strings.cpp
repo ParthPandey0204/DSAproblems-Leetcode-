af4ac62d4b38dce1be89740b50fff532a89df561
Parth Pandey <parth.pandey80045@gmail.com>
2024-11-29 14:43:40 +0530

    Time: 3 ms (51.95%) | Memory: 9.4 MB (71.43%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.length() != t.length()) return false; // Strings of different lengths cannot be isomorphic
-
-        // Maps to store the mapping from `s` to `t` and `t` to `s`
-        unordered_map<char, char> mapST; // Maps characters from `s` to `t`
-        unordered_map<char, char> mapTS; // Maps characters from `t` to `s`
-
+        if (s.length() != t.length()) return false;
+        unordered_map<char, char> mapST;
+        unordered_map<char, char> mapTS; 
         for (int i = 0; i < s.length(); i++) {
             char sChar = s[i];
             char tChar = t[i];
-
-            // Check if there is already a mapping for `sChar` in `mapST`
             if (mapST.count(sChar) && mapST[sChar] != tChar) {
-                return false; // Inconsistent mapping from `s` to `t`
+                return false; 
             }
-
-            // Check if there is already a mapping for `tChar` in `mapTS`
             if (mapTS.count(tChar) && mapTS[tChar] != sChar) {
-                return false; // Inconsistent mapping from `t` to `s`
+                return false; 
             }
-
-            // Establish the mappings
             mapST[sChar] = tChar;
             mapTS[tChar] = sChar;
         }
 
-        return true; // All characters map consistently
+        return true; 
     }
 };
