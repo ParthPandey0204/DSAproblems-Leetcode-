c360cfe42f90b3892a943ed41a736d241d468162
Parth Pandey <parth.pandey80045@gmail.com>
2024-12-03 04:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.34%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) return 0;
-        
-        goal += goal;
-        
-        int a = 0, b = 0, res = 0, prev = 0;
-        while (a < s.size() && b < goal.size()) {
-            if (s[a] == goal[b]) {
-                a++;
-                if(a >= s.size()) return 1;
-            } else {
-                a = 0;
-                b = prev;
-                prev++;
-            }
-            b++;
-        }
-        return 0;
+        if (s.size() != goal.size()) return false;
+        // Concatenate goal with itself and check if s is a substring of goal + goal
+        return (goal + goal).find(s) != string::npos;
     }
 };
