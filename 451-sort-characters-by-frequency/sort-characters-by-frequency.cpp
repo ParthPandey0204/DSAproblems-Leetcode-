0cd03d854a9f434cc9ec185674e7a9228f91f3fd
Parth Pandey <parth.pandey80045@gmail.com>
2024-12-04 00:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (28.20%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char, int> Counter;
-        for(int i = 0; i < s.size(); ++i) Counter[s[i]]++;
-        priority_queue<pair<int, char>> pq; // { occ, char }
-        for(auto it = Counter.begin(); it != Counter.end(); ++it) {
-            pq.push({it->second, it->first});
-        }
-        string res = "";
-        while (!pq.empty()) {
-            auto [occ, ch] = pq.top();
-            pq.pop();
-            res.append(occ, ch);
-        }
-        return res;
+     map<char,int>Counter;
+     for(int i= 0;i<s.size();i++){
+        Counter[s[i]]++;
+     }
+     priority_queue<pair<int,char>>pq;
+      for (std::map<char, int>::iterator it = Counter.begin(); it != Counter.end(); ++it){
+        pq.push(make_pair(it->second,it->first));
+     }
+     string res = "";
+     while(!pq.empty()){
+        pair<int,char>top = pq.top();
+        pq.pop();
+        res.append(top.first,top.second);
+     }
+     return res;
     }
+
 };
