39fda70a273c27a8dda1b23307e38147776acb97
Parth Pandey <parth.pandey80045@gmail.com>
2026-01-06 18:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.57%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
    double myPow(double x, int n) {
-    long long N = n;               // Use long long to handle INT_MIN
+    long long N = n;
     bool is_negative = false;
-    if (N < 0) {
+    if(N <0){
         is_negative = true;
         N = -N;
     }
     double result = 1.0;
-    while (N > 0) {
-        if (N % 2 == 1) {
-            result *= x;
+    while(N>0){
+        if(N % 2 == 1){
+            result = result *x;  
         }
-        x *= x;
-        N /= 2;
+        x = x * x;
+        N = N/2;
     }
-    if (is_negative) {
-        result = 1.0 / result;
+    if (is_negative){
+        result = 1.0/result;
     }
     return result;
 }
